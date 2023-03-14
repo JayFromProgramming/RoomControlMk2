a4908e26d584516c00994246e833185f84c0e29c
JayFromProgramming <ajsweene@mtu.edu>
2023-03-14 13:17:45 -0400

    Switched to loguru for logging

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from loguru import logger as logging
 import sys
+import os
+
+# Create a logs folder if it doesn't exist and make sure its permissions are correct
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+    os.chmod("logs", 0o777)
+
 logging.remove()
 logging.add(sys.stdout, level="INFO")
 logging.add("logs/{time}.log", rotation="1 week", retention="1 hour", compression="zip", level="INFO")
