8159ea1e3f8a01a3e794a9b0712c8791bdad4c06
Jay <ajsweene@mtu.edu>
2023-09-02 19:56:30 -0400

    adding weather support

--- Modules/RoomControl/API/pages/js/weather_loader.js
+++ Modules/RoomControl/API/pages/js/weather_loader.js
@@ -15,14 +15,14 @@ function timestamp_to_time (ts) {
     return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 }
 
-function time_delta_to_string (timestamp) {
+function time_delta_to_stamp (timestamp, inverse = false) {
     // Create a time delta string from a timestamp
     // The delta is formatted as T-00:00 or T+00:00
     // The timestamp is in minutes
 
     var delta = timestamp - (Date.now() / 1000);
     var sign = '-';
-    if (delta < 0) {
+    if (delta < 0 && !inverse) {
         sign = '+';
         delta = delta * -1;
     }
@@ -31,7 +31,6 @@ function time_delta_to_string (timestamp) {
     hours = ('0' + hours).slice(-2);
     var minutes = Math.floor((delta - (hours * 3600)) / 60);
     return 'T' + sign + hours + ':' + minutes;
-
 }
 
 function visibility_to_string(visibility) {
@@ -67,11 +66,11 @@ function update_weather () {
             if (data["sunrise"] > data["sunset"]) {
                 weather_box.append('<tr><td>Sunrise:</td><td align="right"> ' +
                     timestamp_to_time(data["sunrise_time"]) +'\n' +
-                    time_delta_to_string(data["sunrise_time"]) + '</td></tr>');
+                    time_delta_to_stamp(data["sunrise_time"]) + '</td></tr>');
             } else {
                 weather_box.append('<tr><td>Sunset:</td><td align="right"> ' +
                     timestamp_to_time(data["sunset_time"]) + '\n' +
-                    time_delta_to_string(data["sunset_time"]) + '</td></tr>');
+                    time_delta_to_stamp(data["sunset_time"]) + '</td></tr>');
             }
             // Add a last updated timestamp
             weather_box.append('<tr><td>Last Update:</td><td align="right"> ' +
