413ba647582231d24b4367b2317cf5df65a1ace7
Jay <ajsweene@mtu.edu>
2024-05-14 09:00:54 -0400

    Better frontend name handling

--- Modules/RoomControl/API/name_handler.py
+++ Modules/RoomControl/API/name_handler.py
@@ -12,11 +12,19 @@ class NameHandler:
         self.database.create_table("name_relations", {"name": "TEXT", "object_id": "TEXT"})
 
     def get_name(self, object_id):
-        row = self.database.get_table("name_relations").get_row(object_id=object_id)
+        row = self.database.run("SELECT * FROM name_relations WHERE object_id = ?", (object_id,)).fetchone()
         if row:
-            return row['name']
+            return row[0]
         # logging.warning(f"{object_id} did not have a front facing name in name_relations")
         return object_id
 
     def set_name(self, object_id, name):
-        self.database.get_table("name_relations").add(name=name, object_id=object_id)
+        current_name = self.get_name(object_id)
+        if current_name == object_id:
+            logging.info(f"Adding {object_id} name {name}")
+            self.database.run("INSERT INTO name_relations (name, object_id) VALUES (?, ?)", (name, object_id))
+        else:
+            logging.info(f"Updating {object_id} name from {current_name} to {name}")
+            self.database.run("UPDATE name_relations SET name = ? WHERE object_id = ?", (name, object_id))
+
+
