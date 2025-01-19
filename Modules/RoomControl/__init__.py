459c82cc1c3407801bfce93c73a168b064f01143
JayFromProgramming <ajsweene@mtu.edu>
2025-01-19 12:14:33 -0500

    Updating requirements.txt

--- Modules/RoomControl/__init__.py
+++ Modules/RoomControl/__init__.py
@@ -80,17 +80,12 @@ class RoomController:
     def __init__(self, db_path: str = "room_data.db"):
         logging.info("Starting RoomController")
         self.database = Database(db_path)
-        # self.disk_database = sqlite3.connect(db_path)
-        try:
-            logging.info(f"Creating backup database at {db_path}.bak")
-            self.backup_database = sqlite3.connect(f"{db_path}.bak")
-            # self.disk_database.backup(target=self.backup_database, progress=database_backup)
-        except sqlite3.OperationalError:
-            logging.warning("Backup database is already in use, skipping backup")
+        self.backup_database(db_path)
+        # Enable WAL mode
+        self.database.execute("PRAGMA journal_mode=WAL")
         # self.load_database(self.database, db_path)
         self.init_database()
         # self.background_database_sync()
-
         # Find all subclasses of RoomModule and create an instance of them
         self.controllers = []
         self.room_objects = []
@@ -109,6 +104,16 @@ class RoomController:
             except Exception as e:
                 logging.error(f"Error creating instance of {room_module.__name__}: {e}")
                 logging.exception(e)
+
+    @background
+    def backup_database(self, db_path: str = "room_data.db"):
+        try:
+            logging.info(f"Creating backup database at {db_path}.bak")
+            backup_database = sqlite3.connect(f"{db_path}.bak")
+            self.database.backup(target=backup_database, pages=100,
+                                 progress=database_backup)
+        except sqlite3.OperationalError:
+            logging.warning("Backup database is already in use, skipping backup")
 
     def load_database(self, target, source: str):
         logging.info(f"Loading database from {source}")
