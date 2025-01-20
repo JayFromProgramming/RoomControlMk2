15f1508247607191c402b2ab8db91303cf109b22
JayFromProgramming <ajsweene@mtu.edu>
2025-01-20 08:40:09 -0500

    Updating requirements.txt

--- Modules/RoomControl/SatelliteAPI/SatelliteInterface.py
+++ Modules/RoomControl/SatelliteAPI/SatelliteInterface.py
@@ -294,6 +294,8 @@ class Satellite:
             logging.warning(f"Failed to send event to {self.name} due to a connection error")
         except aiohttp.client.InvalidURL:
             logging.warning(f"Failed to send event to {self.name} due to an invalid URL")
+        except aiohttp.client.ClientResponseError as e:
+            logging.warning(f"{self.name} returned an invalid response to an event message: {e}")
         except Exception as e:
             logging.error(f"Error sending event to {self.name}: {e}")
             logging.exception(e)
