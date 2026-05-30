1ab4fe68fc6a5ebc9e7c1a5d6082a8ad0af8b894
jay <ajsweene@mtu.edu>
2026-05-30 11:35:16 -0700

    Add logging for non-raspbian platform to skip module filtering

--- Modules/RoomControl/__init__.py
+++ Modules/RoomControl/__init__.py
@@ -107,6 +107,7 @@ class RoomController:
         for room_module in RoomModule.__subclasses__():
             logging.info(f"Creating instance of {room_module.__name__}")
             if "raspbian" not in sys.platform:
+                logging.info(f"Running on non-raspbian platform, skipping module filtering")
                 if (len(self.only_modules) > 0 and room_module.__name__ not in self.only_modules
                         and room_module.__name__ not in self.required_modules):
                     logging.info(f"Skipping {room_module.__name__} because it is not in only_modules")
